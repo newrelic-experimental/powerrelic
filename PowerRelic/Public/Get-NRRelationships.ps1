5d4315f3b24ce745f3789c8b3639d975c741c013
zackm <zack.mutchler@gmail.com>
2020-08-18 08:56:01 -0500

    fix: indentation

--- PowerRelic/Public/Get-NRRelationships.ps1
+++ PowerRelic/Public/Get-NRRelationships.ps1
@@ -12,39 +12,39 @@
   [REQUIRED] The GUID of the target entity.
   
   .OUTPUTS
-domain: The entity's domain
-entityType: The specific type of entity (Domain + Type)
-name: The entity's name
-permalink: The permalink URL of this entity
-relationships:
-    .source:
-        .entity:
-            .domain: This source entity's domain
-            .entityType: The specific type of this source entity (Domain + Type)
-            .guid: This source entity's GUID
-            .name: This source entity's name
-            .permalink: This source entity's permalink
-            .reporting: Binary indicator on whether this source entity is reporting data
-            .tags:
-                .key: Key name for this tag
-                .values: Value(s) for this tag key
-            .type: This source entity's type
-    .target:
-        .entity:
-            .domain: This target entity's domain
-            .entityType: The specific type of this target entity (Domain + Type)
-            .guid: This target entity's GUID
-            .name: This target entity's name
-            .permalink: This target entity's permalink
-            .reporting: Binary indicator on whether this target entity is reporting data
-            .tags:
-                .key: Key name for this tag
-                .values: Value(s) for this tag key
-            .type: This target entity's type
-tags:
-    .key: Key name for this tag
-    .values: Value(s) for this tag key
-type: This entity's type
+  domain: The entity's domain
+  entityType: The specific type of entity (Domain + Type)
+  name: The entity's name
+  permalink: The permalink URL of this entity
+  relationships:
+      .source:
+          .entity:
+              .domain: This source entity's domain
+              .entityType: The specific type of this source entity (Domain + Type)
+              .guid: This source entity's GUID
+              .name: This source entity's name
+              .permalink: This source entity's permalink
+              .reporting: Binary indicator on whether this source entity is reporting data
+              .tags:
+                  .key: Key name for this tag
+                  .values: Value(s) for this tag key
+              .type: This source entity's type
+      .target:
+          .entity:
+              .domain: This target entity's domain
+              .entityType: The specific type of this target entity (Domain + Type)
+              .guid: This target entity's GUID
+              .name: This target entity's name
+              .permalink: This target entity's permalink
+              .reporting: Binary indicator on whether this target entity is reporting data
+              .tags:
+                  .key: Key name for this tag
+                  .values: Value(s) for this tag key
+              .type: This target entity's type
+  tags:
+      .key: Key name for this tag
+      .values: Value(s) for this tag key
+  type: This entity's type
 
   .EXAMPLE
   PS> Get-NRRelationships -PersonalAPIKey 'NRAK-123456789ABCDEFGHIJKLMNOPQR' -GUID A1B2C3D4E5F6G7H8I9J0
